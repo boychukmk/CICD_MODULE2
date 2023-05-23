4f3d8e7a496914c9f638c3048b75160bb16be904
Max Boichuk <boychuk.mk@gmail.com>
2023-05-23 15:21:13 +0300

    tests

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
-
+from gallery.models import Image
 # Create your tests here.
+class ModelImgTests(TestCase):
+    def setup(self):
+        Image.objects.create(title="Test", image='media/gallery_images/teddy.jpeg',
+                             created_date="2023-05-20", age_limit=1, categories= 'Dogs')
+
+    def test_get(self):
+        img = Image.objects.get(title="Test")
+        self.assertTrue(img)
+
+    def test_age(self):
+        img = Image.objects.get(title="Test")
+        self.assertTrue(img.age_limit == 1)
